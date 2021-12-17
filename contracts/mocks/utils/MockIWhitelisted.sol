95f7c01ee90131345d1a9f2e8055a9e6b7c54969
silvereh <silvere.heraudeau@gmail.com>
2021-12-17 04:18:13 -0700

    Added IWhitelisted

---
+++ contracts/mocks/utils/MockIWhitelisted.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+/**
+* Author: Lambdalf the White
+*/
+
+pragma solidity 0.8.10;
+
+import "../../utils/IWhitelisted.sol";
+
+contract MockIWhitelisted is IWhitelisted {
+	constructor() {}
+
+	function setWhitelist( bytes32 root_, uint256 passMax_ ) public {
+		_setWhitelist( root_, passMax_ );
+	}
+
+	function checkWhitelistAllowance( address account_, bytes32 proof_, bool flag_ ) public view returns ( uint256 ) {
+		return _checkWhitelistAllowance( account_, proof_, flag_ );
+	}
+
+	function consumeWhitelist( address account_, uint256 qty_, bytes32 proof_, bool flag_ ) public {
+		_consumeWhitelist( account_, qty_, proof_, flag_ );
+	}
+}
